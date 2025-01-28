1787649bff6379525784c84b5a67ee5a27aa0fda
mdsajalali <mdsajalali2020@gmail.com>
2025-01-28 23:57:29 +0600

    add: prompt functionality

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -1,6 +1,59 @@
-const generateImage = async (req, res) => {};
+const { default: Together } = require("together-ai");
+const ImageModal = require("../model/image.model");
+const { uploadBase64toImage } = require("../utils");
+const apiKey = process.env.TOGETHER_API_KEY;
+const togetherAIModel = process.env.TOGETHER_AI_MODEL;
 
-const getImages = async (req, res) => {};
+const generateImage = async (req, res) => {
+  try {
+    const together = new Together({ apiKey });
+    const { prompt } = req.body;
+
+    let response = await together.images.create({
+      prompt,
+      model: togetherAIModel,
+      width: 1024,
+      height: 768,
+      steps: 4,
+      n: 1,
+      response_format: "b64_json",
+    });
+    const base64Image = response?.data[0]?.b64_json;
+
+    // upload the base64 image to the storage
+    const imageUrl = await uploadBase64toImage(base64Image);
+    // Insert into the DB
+    const newImage = new ImageModal({ prompt, imageUrl });
+    await newImage.save();
+
+    return res.status(200).json({ imageUrl });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getImages = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const totalImages = await ImageModal.countDocuments();
+
+    const images = await ImageModal.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 });
+
+    const data = {
+      images,
+      totalPages: Math.ceil(totalImages / limit),
+      currentPage: page,
+    };
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   generateImage,
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+
 const imageRoutes = require("./routes/image.route.js");
 
 // Middleware setup
