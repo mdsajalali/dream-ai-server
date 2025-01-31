a6006639ea8b249499033f34dc58807ddb1fcc57
mdsajalali <mdsajalali2020@gmail.com>
2025-01-31 17:13:10 +0600

    add: user get, post functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,9 +1,46 @@
-const createUser = (req, res) => {
-  res.send("hello post user");
+const bcrypt = require("bcryptjs");
+const UserModel = require("../model/user.modal");
+
+// Create a new user
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+      confirmPassword: hashedPassword,
+    });
+
+    // Save user to database
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
-const getUsers = (req, res) => {
-  res.send("Hello get User");
+// Get all users
+const getUsers = async (req, res) => {
+  try {
+    // const users = await UserModel.find({}, { password: 0 });
+    const users = await UserModel.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 module.exports = {
