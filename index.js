0083bc8258330aaf85836316690726d6a8862ce5
mdsajalali <mdsajalali2020@gmail.com>
2025-02-01 23:34:02 +0600

    completed

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const connectDB = require("./config/db");
 connectDB();
 
 const imageRoutes = require("./routes/image.route.js");
-const userRoutes = require("./routes/user.route.js");
-const favoriteRoutes = require("./routes/favorite.route.js");
 
 // Middleware setup
 app.use(express.json());
@@ -19,8 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1", imageRoutes);
-app.use("/api/v1", userRoutes);
-app.use("/api/v1", favoriteRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- routes/favorite.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express"); 
-const { createFavorite, myFavorites, removeFromFavorite } = require("../controller/favorite.controller");
-const router = express.Router();
-
-router.post("/favorite", createFavorite);
-router.get("/favorite/mine", myFavorites);
-router.delete("/favorite/:imageId", removeFromFavorite);
-
-module.exports = router;
