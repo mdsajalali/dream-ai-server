07164efe8b32bfb7701b70f721979f2738198432
mdsajalali <mdsajalali2020@gmail.com>
2025-04-19 00:43:14 +0600

    chore

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ const favoriteRoutes = require("./routes/favorite.route.js");
 
 app.use(express.json());
 app.use(
-  cors({
-    origin: ["https://dream-aii.vercel.app"],
-    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
-    credentials: true,
-  })
-);
-
-app.use(
   fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
   })
 );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", ["Authorization"]],
+  })
+);
+
+
 
 app.get("/api/v1", (req, res) => {
   res.send("Welcome to DreamAI!");
