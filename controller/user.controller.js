b5d1bf1a1e1207997c040f5760747bf04241b530
mdsajalali <mdsajalali2020@gmail.com>
2026-02-04 20:24:28 +0600

    refact: create user response data handling

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -26,7 +26,10 @@ const createUser = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json({
+      message: "User created successfully",
+      userId: newUser._id,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
